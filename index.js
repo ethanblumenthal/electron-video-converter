4d8e683e817a3f06da93a651bcb256897d8d12f1
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-21 15:40:46 -0500

    retreive video metadata with ffmpeg

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const electron = require('electron')
-const { app, BrowserWindow } = electron
+const ffmpeg = require('fluent-ffmpeg')
+const _ = require('lodash')
+const { app, BrowserWindow, ipcMain } = electron
 
 let mainWindow
 
@@ -10,4 +12,19 @@ app.on('ready', () => {
     webPreferences: { backgroundThrottling: false }
   })
   mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+})
+
+ipcMain.on('videos:added', (event, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        video.duration = metadata.format.duration,
+        video.format = 'avi'
+        resolve(video)
+      })
+    })
+  })
+  Promise.all(promises).then(results => {
+    mainWindow.webContents.send('metadata:complete', results)
+  })
 })
